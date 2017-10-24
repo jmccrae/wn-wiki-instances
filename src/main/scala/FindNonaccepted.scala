963f526fac1c1b2860e5edb9a0568fd8711b1c01
John P. McCrae <john@mccr.ae>
2017-10-24 16:56:29 +0100

    Add final mappings

--- src/main/scala/FindNonaccepted.scala
+++ src/main/scala/FindNonaccepted.scala
@@ -2,22 +2,50 @@ import argonaut._
 import Argonaut._
 
 object FindNonaccepted {
+  val commaSep = "(.*), (.*)".r
+  val disambig = "(.*) \\((.*)\\)".r
+
   def main(args : Array[String]) {
     
-    val wiki = io.Source.fromFile("wordnet.json").mkString("").
+    val wn = io.Source.fromFile("wordnet.json").mkString("").
       decodeOption[Map[String, WordNetEntry]].get
 
     val accepted = io.Source.fromFile("accepted.tsv").getLines.map({ line =>
       val elems = line.split("\t")
-      elems(1)
+      (elems(0), elems(1))
     }).toSet
 
-    wiki.filter({
-      case (id, wne) => wne.isInstance && !accepted.contains(id)
+    val acceptedWn = accepted.map(_._2)
+
+    val out = new java.io.PrintWriter("manual-check.tsv")
+
+    out.println("WordNet ID\tLemmas\tDefinition\tWikiID\tType")
+    accepted.filter({
+      case (wikiId, wnId) => {
+        val lemmas = wn(wnId).lemmas.map(_.toLowerCase)
+        val defn = wn(wnId).defn.toLowerCase
+        wikiId.toLowerCase match {
+          case x if lemmas.contains(x) => false
+          case commaSep(x,y) if lemmas.contains(x) && defn.contains(y) => false
+          case disambig(x,y) if lemmas.contains(x) && defn.contains(y) => false
+          case _ => true
+        }
+      }
+    }).foreach({
+      case (wikiId, wnId) => {
+        val wne = wn(wnId)
+        out.println(s"$wnId\t${wne.lemmas.mkString(",")}\t${wne.defn.replaceAll("\t", " ")}\t$wikiId")
+      }
+    })
+
+
+    wn.filter({
+      case (id, wne) => wne.isInstance && !acceptedWn.contains(id)
     }).foreach({
       case (id, wne) =>
-        println(s"""$id\t${wne.lemmas.mkString(",")}\t${wne.defn}""")
+        out.println(s"""$id\t${wne.lemmas.mkString(",")}\t${wne.defn}""")
     })
     
+    out.close
   }
 }
